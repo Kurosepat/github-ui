5c27287fcf36e5ce8a174cb8da2ff9fc936f8722
Kurosepat <yasuhisa.kurose@gmail.com>
2025-05-27 14:58:53 -0400

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.post('/relay', upload.any(), async (req, res) => {
       formData.append(file.fieldname, file.buffer, file.originalname);
     }
 
-    const makeWebhookURL = 'https://hook.us2.make.com/ggm2mlmimmrpnwyz5p21mhhd0dmk29p7';
+    const makeWebhookURL = 'https://hook.us2.make.com/2oi6xu9uwoep5xbj7y8ir4qq4c6jvctf';
 
     const response = await fetch(makeWebhookURL, {
       method: 'POST',
